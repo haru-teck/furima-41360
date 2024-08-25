d122975554e4904b044c4d74b9a62b70737794ea
haru-teck <ginchatoku1894@gmail.com>
2024-08-25 16:18:49 +0900

    コードレビュー2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :title, :explanation, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :ship_day_id, :price, :description).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :explanation, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :ship_day_id, :price).merge(user_id: current_user.id)
   end
 end
 
